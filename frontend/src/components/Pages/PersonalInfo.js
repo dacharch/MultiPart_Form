f8d3b29802dcffdcd796c9d6ffd57f535b2b81db
Neeraj Kumar <nk35715945@gmail.com>
2024-06-05 23:19:31 +0530

    Some Files are added

--- frontend/src/components/Pages/PersonalInfo.js
+++ frontend/src/components/Pages/PersonalInfo.js
@@ -1,9 +1,29 @@
-import { Container } from '@mui/material'
+import { Container, TextField,Grid} from '@mui/material'
+import { FormState } from '../../context/StateProvider';
 import React from 'react'
 
 const PersonalInfo = () => {
+  const {
+    fullName,
+    setFullName,
+    dateofBirth,
+
+  } = FormState();
   return (
      <Container>
+          <Grid container spacing={2}>
+             <Grid item xs={12} >
+                 <TextField
+                   fullWidth
+                   label="Full Name"
+                   name="Full Name"
+                   value={fullName} 
+                >
+                   
+                 </TextField>
+             </Grid>
+
+          </Grid>
       
      </Container>
   )
--- frontend/src/context/StateProvider.js
+++ frontend/src/context/StateProvider.js
@@ -7,6 +7,12 @@ const FormContext = createContext( ) ;
 const StateProvider = ({children}) => {
     const [fullName,setFullName] =useState("");
     const [dateOfBirth,setDateOfBirth] =useState(new Date());
+    const [email,setEmail] = useState("") ;
+    const [phoneNumber, setPhoneNumber] = useState("") ;
+    const [programmingLanguage,setProgrammingLanguage] = useState() ;
+    const [experience,setExperience]= useState("") ;
+    const [interestedArea,setInterestedArea] =useState([]) ;
+    const [selectedFile,setSelectedFile] = useState(null) ;
 
   
     return (
