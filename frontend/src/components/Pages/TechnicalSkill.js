f6e3739154fbe0be4e15af86d388541bffee0aa7
Neeraj Kumar <nk35715945@gmail.com>
2024-06-15 12:42:39 +0530

    Update Check box

--- frontend/src/components/Pages/TechnicalSkill.js
+++ frontend/src/components/Pages/TechnicalSkill.js
@@ -1,6 +1,6 @@
 import { Checkbox, FormControlLabel, FormLabel, Grid, MenuItem, TextField,Box } from '@mui/material'
 import { FormState } from '../../context/StateProvider';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from "react";
 import React from 'react'
 
 const TechnicalSkill = () => {
@@ -14,6 +14,25 @@ const TechnicalSkill = () => {
     interestedArea,
     setInterestedArea,
   } = FormState() ;
+
+  const handleDevelopmentCheckBoxChange = (name) => (event) => {
+      if (event.target.checked || developmentCheck.length > 1) {
+        setDevelopmentCheck((prevdevelopmentCheck) => {
+          if (event.target.checked) {
+            return [...prevdevelopmentCheck, name];
+          } else {
+            return prevdevelopmentCheck.filter(
+              (checkboxName) => checkboxName !== name
+            );
+          }
+        });
+      }
+  };
+
+  const handleInterestedCheckBoxChange = (name) => (event) =>{
+     
+  }
+
   return (
     <div>
       <Grid container spacing={2}>
@@ -39,6 +58,8 @@ const TechnicalSkill = () => {
             select
             label="Experience"
             name="Experience level"
+            value={experience}
+            onChange={(e) => setExperience(e.target.value)}
             required
           >
             <MenuItem value="Beginner">Beginner</MenuItem>
@@ -50,14 +71,42 @@ const TechnicalSkill = () => {
         <Grid item xs={12}>
           <FormLabel required>Development Stack</FormLabel>
           <Box>
-            <FormControlLabel control={<Checkbox />} label="MEAN" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={developmentCheck.includes("MEAN")}
+                  onChange={handleDevelopmentCheckBoxChange("MEAN")}
+                />
+              }
+              label="MEAN"
+            />
 
-            <FormControlLabel control={<Checkbox />} label="LAMP" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={developmentCheck.includes("MERN")}
+                  onChange={handleDevelopmentCheckBoxChange("MERN")}
+                />
+              }
+              label="MERN"
+            />
 
-            <FormControlLabel control={<Checkbox />} label="MERN" />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={developmentCheck.includes("LAMP")}
+                  onChange={handleDevelopmentCheckBoxChange("LAMP")}
+                />
+              }
+              label="LAMP"
+            />
 
-            <FormControlLabel control={<Checkbox />} label="LAMP" />
-            <FormControlLabel control={<Checkbox />} label="OTHER" />
+          
+
+            <FormControlLabel control={<Checkbox 
+               checked ={developmentCheck.includes("OTHER")}
+               onChange={handleDevelopmentCheckBoxChange("OTHER")}
+            />} label="OTHER" />
           </Box>
         </Grid>
 
@@ -65,14 +114,37 @@ const TechnicalSkill = () => {
           <FormLabel>Interested Area in Task Scheduling</FormLabel>
 
           <Box>
-            <FormControlLabel control={<Checkbox />} label="UI/UX Design" />
+            <FormControlLabel control={<Checkbox
+                 checked={interestedArea.includes("UI/UX Design")}
+                 onChange={handleInterestedCheckBoxChange("UI/UX Design")} 
+            />} label="UI/UX Design" />
 
-            <FormControlLabel control={<Checkbox />} label="Backend Development" />
+            <FormControlLabel
+              control={<Checkbox
+                checked={interestedArea.includes("Backend Development")}
+                onChange={handleDevelopmentCheckBoxChange("Backend Development")}
+             />}
+              label="Backend Development"
+            />
 
-            <FormControlLabel control={<Checkbox />} label="Database Management" />
+            <FormControlLabel
+              control={<Checkbox 
+                checked={interestedArea.includes("Database Management")}
+                onChange={handleInterestedCheckBoxChange("Database Management")}
+              />}
+              label="Database Management"
+            />
 
-            <FormControlLabel control={<Checkbox />} label="Authentication" />
-            <FormControlLabel control={<Checkbox />} label="Real Time Updates" />
+            <FormControlLabel control={<Checkbox
+               checked={interestedArea.includes("Authentication")}
+            />} label="Authentication" />
+            <FormControlLabel
+              control={<Checkbox 
+                checked={interestedArea.includes("Real Time Updates")}
+                onChange={handleInterestedCheckBoxChange("Real-Time Updates")}
+              />}
+              label="Real Time Updates"
+            />
           </Box>
         </Grid>
       </Grid>
