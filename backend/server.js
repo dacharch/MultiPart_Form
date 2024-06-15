d41e4f1a0062d9305aa7eab42c5544d2742bf5c5
Neeraj Kumar <nk35715945@gmail.com>
2024-06-15 16:43:15 +0530

    Schema is ready

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express") ;
-
+const cors = require('cors') ;
+const connectDB = require("./config/db") ;
+const dotenv = require("dotenv") ;
+const fs = require('fs') ;
+const multer = require('multer') ;
+const mongoose = require('mongoose') ;
 const app = express() ;
+
+
 
 app.get('/',(req,res)=>{
     res.send("Hello world") ;
