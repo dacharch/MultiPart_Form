435699c2d8a0918de273f69f2e5a5c5ee369a57b
Neeraj Kumar <nk35715945@gmail.com>
2024-06-15 18:46:58 +0530

    server is ready

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,58 @@ app.get('/',(req,res)=>{
     res.send("Hello world") ;
 })
 
-app.listen(3002,()=>{
+
+app.post("/api/submit",upload.single("file"),async (req,res)=>{
+    const {
+        fullName,
+        dateOfBirth,
+        email,
+        phoneNumber,
+        programmingLangauge,
+        experience,
+        developmentCheck,
+        interestedArea,
+    } = JSON.parse (req.body.userDate) ;
+
+    const file = req.file ;
+
+    Personal.create({
+        fullName,
+        dateOfBirth,
+        email,
+        phoneNumber,
+    }) ;
+
+    Skills.create({
+        programmingLangauge,
+        experience,
+        developmentCheck,
+        interestedArea,
+    })
+
+    if(!file){
+         return res.status(400).json({message:"No file uploaded"}) ;
+    }
+
+    try{
+         const file = new File ({
+             filename: req.body.filename,
+         }) ;
+
+         await file.save() ;
+
+         res.json({message: "Form Successfully Submitted"}) ;
+
+    }catch(error){
+        console.error("Error uploading file",error) ;
+        res.status(500).json({message: "Internal server error"}) ;
+    }
+
+    
+})
+
+const PORT =  process.env.PORT  || 8080 ;
+
+app.listen(PORT,()=>{
     console.log("server is running") ;
 })
