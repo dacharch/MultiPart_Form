b7bea40ed723d010da4b4c252cf8d16237d3ddc0
Neeraj Kumar <nk35715945@gmail.com>
2024-06-15 20:51:11 +0530

    bug removes

--- frontend/src/components/Pages/UploadSection.js
+++ frontend/src/components/Pages/UploadSection.js
@@ -1,33 +1,77 @@
-import React from 'react'
-import {Button,Typography} from "@mui/material" ;
+import React, { useRef, useState } from 'react'
+import {Button,Snackbar,Typography,Alert} from "@mui/material" ;
 import { GrCloudUpload } from "react-icons/gr";
+import { FormState } from "../../context/StateProvider";
+
 
 
 const UploadSection = () => {
+  const [fileName,setFileName] = useState('')  ;
+  const fileInputRef = useRef(null) ;
+  const [error,setError] = useState(null) ;
+
+  const {setSelectedFile} = FormState() ;
+  
+  const handleBrowse = ()=>{
+    fileInputRef.current.click() ;
+  }
+
+  const fileUpload =(e) =>{
+    const file = e.target.files[0] ;
+
+    setFileName(file.name) ;
+    if(file){
+       if(file.size <=5 *1024 * 1024){
+          setSelectedFile(file) ;
+       }else{
+         setError("File size exceeds 5 MB limit") ;
+         e.target.value = null ;
+         setSelectedFile(null) ;
+       }
+    }
+  }
+
+  const handleCloseSnackbar = ()=>{
+     setError(null) ;
+  }
+  
   return (
-     <div className='upload_section'>
-        <div className='upload_container'>
-             <input 
-                type="file"
-                accept='.doc, .docx, .pdf'
-                hidden
-             />
+    <div className="upload_section">
+      <div className="upload_container">
+        <input type="file" accept=".doc, .docx, .pdf" hidden />
 
-             <GrCloudUpload />
-             <Button variant="contained" >
-                 Browse
-             </Button>
+        <GrCloudUpload />
+        <Button 
+         variant="contained"
+         onClick={handleBrowse}
 
-             <br/>
+        >
+         Browse
+        </Button>
+              
+        <br />
+        <Typography>FileName: </Typography>
+        <Typography>PDF/ DOCX</Typography>
+        <Typography>File Size : 5 MB</Typography>
 
-             <Typography>FileName:  </Typography>
-             <Typography>PDF/ DOCX</Typography>
-             <Typography>File Size : 5 MB</Typography>
+        <Snackbar
+          open={error !== null}
+          autoHideDuration={5000}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert
+            elevation={6}
+            variant="filled"
+            onClose={handleCloseSnackbar}
+            severity='error'
+          >
+             {error}
+          </Alert>
 
-        </div>
-
-     </div>
-  )
+        </Snackbar>
+      </div>
+    </div>
+  );
 }
 
 export default UploadSection
